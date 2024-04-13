e7fcc265166156232155ab255a9c7734751c5bd8
Evanwutx <94091041+Evanwutx@users.noreply.github.com>
2024-04-13 16:51:01 -0500

    shop cycle

--- Shop.gd
+++ Shop.gd
@@ -4,6 +4,7 @@ extends StaticBody3D
 
 var curItem = 0
 var total = 3
+var inShop = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,14 +13,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float):
-	pass
+	if inShop == true:
+		if Input.is_action_just_pressed("ui_right"):
+			cameras[curItem].set_priority(0)
+			curItem += 1
+			curItem = curItem % total
+			cameras[curItem].set_priority(20)
+		if Input.is_action_just_pressed("ui_left"):
+			cameras[curItem].set_priority(0)
+			curItem -= 1
+			curItem = curItem % total
+			cameras[curItem].set_priority(20)
+		if Input.is_action_just_pressed("ui_cancel"):
+			inShop = false
+			
 
 func _on_area_3d_body_entered(body):
-	print("entered shop")
+	print("Entered Shop")
+	inShop = true
 	cameras[0].set_priority(20)
 	
 
 func _on_area_3d_body_exited(body):
-	print("exited shop")
-	cameras[0].set_priority(0)
-	print(cameras[0].get_priority())
+	print("Exited Shop")
