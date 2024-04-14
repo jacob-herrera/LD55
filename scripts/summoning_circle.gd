b3968299a611ad71ced423571d179f6228b84b53
ardie01 <arman.heydarian01@gmail.com>
2024-04-13 21:00:14 -0500

    Teleport Noise

--- scripts/summoning_circle.gd
+++ scripts/summoning_circle.gd
@@ -3,6 +3,7 @@ class_name SummonCircle
 
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite3D = $Sprite3D
+@onready var teleport: AudioStreamPlayer3D = $Teleport
 
 func _ready() -> void:
 	stop_anim()
@@ -10,7 +11,14 @@ func _ready() -> void:
 func start_anim() -> void:
 	sprite.visible = true
 	anim_player.play("summon_spin")
+	play_teleport()
 
 func stop_anim() -> void:
 	anim_player.play("RESET")
 	sprite.visible = false
+
+func play_teleport() -> void:
+	if !teleport.playing:
+		await get_tree().create_timer(1.0).timeout
+		teleport.play()
+	
