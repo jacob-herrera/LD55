47bda7c4da8fe513b64be51e8d0fb13712b7fc7c
Jacob Herrera <jacoblherrera@outlook.com>
2024-04-13 20:45:13 -0500

    dev room switcher

--- scripts/autoloads/game_coordinator.gd
+++ scripts/autoloads/game_coordinator.gd
@@ -4,13 +4,13 @@ class_name GameCoordinator
 @onready var fill: ColorRect = $CanvasLayer/BlackFill
 @onready var anim_player: AnimationPlayer = $CanvasLayer/AnimationPlayer
 
-enum GameState {
+enum Room {
 	HUB,
-	COMBAT
+	ROOM_1,
 }
 
-const HUB_TELE := Vector3(0,0,0)
-const LEVEL_1_TELE := Vector3(32, 0, 0)
+const ROOM_HUB_TELE := Vector3(0,0,0)
+const ROOM_1_TELE := Vector3(32, 0, 0)
 
 const PLAYER_CAMERA_DEFAULT_TWEEN: float = 0.5
 const FADE_DURATION: float = 10.0 / 30.0
@@ -18,13 +18,37 @@ const PLAYER_SUMMON_DURATION = 6.0
 const HUB_TIME: float = 10.0
 const COMBAT_TIME: float = 10.0
 
-static var state: GameState = GameState.HUB
+static var current_room: Room = Room.HUB
 static var current_round: int = 0
 static var time: float = HUB_TIME
 
 static var DEV_timer_paused: bool = false
 
+func DEV():
+	if Input.is_action_just_pressed("dev_pause_timer"):
+		DEV_timer_paused = not DEV_timer_paused
+	if Input.is_action_just_pressed("dev_goto_hub"):
+		goto_room(Room.HUB)
+	if Input.is_action_just_pressed("dev_goto_room1"):
+		goto_room(Room.ROOM_1)
+		
+func goto_room(target_room: Room) -> void:
+	current_room = target_room
+	match target_room:
+		Room.HUB:
+			time = HUB_TIME
+			globals.character.global_position = ROOM_HUB_TELE
+		Room.ROOM_1:
+			time = COMBAT_TIME
+			globals.character.global_position = ROOM_1_TELE
+	anim_player.play("fade_in")	
+	globals.character.summon_circle.stop_anim()
+	globals.character.camera.tween_parameters.duration = PLAYER_CAMERA_DEFAULT_TWEEN
+	ui.particles_off()
+
 func _process(delta: float) -> void:
+	DEV()
+	
 	if DEV_timer_paused == false:
 		time -= delta
 	if time <= FADE_DURATION:
@@ -33,20 +57,12 @@ func _process(delta: float) -> void:
 		globals.character.summon_circle.start_anim()
 	if time <= 3:
 		ui.particles_on()
+		
 	if time <= 0:
-		# Reset tween
-		match state:
-			GameState.HUB:
-				state = GameState.COMBAT
-				globals.character.global_position = LEVEL_1_TELE
-				time = COMBAT_TIME
-			GameState.COMBAT:
-				state = GameState.HUB
-				globals.character.global_position = HUB_TELE
-				time = HUB_TIME
-				current_round += 1
-		anim_player.play("fade_in")	
-		globals.character.summon_circle.stop_anim()
-		globals.character.camera.tween_parameters.duration = PLAYER_CAMERA_DEFAULT_TWEEN
-		ui.particles_off()
+		match current_room:
+			Room.HUB:
+				goto_room(Room.ROOM_1)
+			Room.ROOM_1:
+				goto_room(Room.HUB)
+
 
